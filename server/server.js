a57cb20a97f7ca0980e09f4a32861d5c8dc43358
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-09 22:13:42 -0500

    everything set up and ready to go

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000;
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, () => {
+    console.log("Up and running on port: ", port);
+});
