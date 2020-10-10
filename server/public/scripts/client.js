1390c32eea729bd147606fdc5eadd8102fcb9ef9
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-10 09:44:36 -0500

    created click event and function to bundle two inputs into object, added class=operations to buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,20 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('Hello from jQuery');
+    $('#equals').on('click', crunchNumbers);
+}
+
+// function to calculate inputs
+// incorporates whichever math operation was selected
+function crunchNumbers(){
+    console.log('in crunchNumbers');
+    let answer = {
+        firstNumber: $('#firstNumber').val(),
+        secondNumber: $('#secondNumber').val()
+    }
+    console.log('Inputs as an object', answer);
+    //clears input fields
+    $('#firstNumber').val('');
+    $('#secondNumber').val('');
+    return answer;
 }
